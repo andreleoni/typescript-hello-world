ebdd8e292998af0b3ad855f6aaec31a6a2273fc6
André Leoni <andreluizleoni@gmail.com>
2020-02-29 01:52:55 -0300

    Fix errors and created the docker compose example

--- README.md
+++
@@ -1,1 +0,0 @@
-# typescript-hello-world
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,32 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
 import routes from './routes'
 
 class App {
-    public express: express.Application
+  public express: express.Application
 
-    public constructor() {
-        this.express = express()
+  public constructor () {
+    this.express = express()
 
-        this.middlewares();
-        this.database();
-        this.routes();
-    }
+    this.middlewares()
+    this.database()
+    this.routes()
+  }
 
-    private middlewares():void {
-        this.express.use(express.json())
-        this.express.use(cors())
-    }
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
 
-    private database ():void {
-        mongoose.connect('mongodb://localhost/tsnode',{
-            useNewUrlParser: true
-        })
-    }
+  private database (): void {
+    mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/tsexample`, { useNewUrlParser: true })
+  }
 
-    private routes ():void {
-        this.express.use(routes)
-    }
+  private routes (): void {
+    this.express.use(routes)
+  }
 }
 
 export default new App().express
